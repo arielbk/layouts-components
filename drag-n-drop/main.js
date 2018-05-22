aef79ab0e48cfecd473ca5e679587873f2e36eee
arielbk <buchwald.kearney@gmail.com>
2018-05-23 00:13:52 +0200

    works in FF

--- drag-n-drop/main.js
+++ drag-n-drop/main.js
@@ -1,10 +1,15 @@
 // Tutorial by Traversy Media: https://www.youtube.com/watch?v=C22hQKE_32c&t=2s
+// Weirdly this will not work on FireFox - there is an issue with HTML5 draggable
+// This helped: http://mereskin.github.io/dnd/
+// And most of all MDN's documentation on HTML drag n drop API https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
 
 const fill = document.querySelector('.fill');
 const boxes = document.querySelectorAll('.empty');
 
 // Fill listeners
-fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragstart', function(e){
+  e.dataTransfer.setData('text/plain', e.target.id);
+});
 fill.addEventListener('dragend', dragEnd);
 
 // Loop empties and call drag events
@@ -18,7 +23,7 @@ for(const box of boxes) {
 // Drag functions
 function dragStart() {
   this.className += ' hold';
-  setTimeout(() => (this.className = 'invisible'), 0);
+  // setTimeout(() => (this.className = 'invisible'), 0);
 }
 
 function dragEnd() {

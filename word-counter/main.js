bf357723c9c9161a795fea0cd9e687e62ffd0587
arielbk <buchwald.kearney@gmail.com>
2018-05-30 23:58:44 +0200

    persevering with array methods

--- word-counter/main.js
+++ word-counter/main.js
@@ -1,24 +1,33 @@
 /* Aim is to get some practice with array methods, particularly reduce,
 as well as simply working with the DOM */
 
+'use strict';
+
 const textInputField = document.querySelector('#textInput');
 const submitBtn = document.querySelector('#submit');
 const output = document.querySelector('#output');
 
 submitBtn.addEventListener('click', () => {
   const words = textInputField.value
+    .toLowerCase()
     .split(' ')
-    .reduce((allWords, word) => {
-      if (word in allWords) {
-        allWords[word]++;
+    .reduce((totalWords, singleWord, i) => {
+      if (totalWords.length==0) {
+        totalWords[0] = {word: singleWord, frequency: 1};
+      } else if (totalWords.every(element => element['word'] != singleWord)) {
+        let newElement = {word: singleWord, frequency: 1};
+        totalWords[i] = newElement;
       } else {
-        allWords[word] = 1;
+        totalWords.forEach(element => {if (element['word'] == singleWord) element['frequency']++});
       }
-      return allWords;
-    }, []);
-  let content = '';
-  for (word in words) {
-    content += '<li>'+word+' : '+words[word]+'</li>';
-  }
-  output.innerHTML = content;
+      return totalWords;
+    }, [])
+    .sort((a,b) => b['frequency']-a['frequency']);
+
+  // let content = '';
+  // for (var word in words) {
+  //   content += '<li>'+word+' - '+words[word]+'</li>';
+  // }
+  // output.innerHTML = content;
+  console.log(words);
 });

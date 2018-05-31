286babdd490c8687067269a4e002437b4d155638
arielbk <buchwald.kearney@gmail.com>
2018-05-31 16:46:08 +0200

    completed my word counter

--- word-counter/main.js
+++ word-counter/main.js
@@ -3,31 +3,48 @@ as well as simply working with the DOM */
 
 'use strict';
 
-const textInputField = document.querySelector('#textInput');
+// define all selectors
+const textInputField = document.querySelector('#text-input');
 const submitBtn = document.querySelector('#submit');
 const output = document.querySelector('#output');
+const topNumber = document.querySelector('#top-number');
+const outputTable = document.querySelector('#output-table');
 
+// hide output table to start
+outputTable.style.display = 'none';
+
+// when the submit button is clicked
 submitBtn.addEventListener('click', () => {
   const words = textInputField.value
+    // this is where the magic happens: chained array methods
     .toLowerCase()
     .split(' ')
-    .reduce((totalWords, singleWord, i) => {
-      if (totalWords.length==0) {
-        totalWords[0] = {word: singleWord, frequency: 1};
-      } else if (totalWords.every(element => element['word'] != singleWord)) {
+    // remove all non alpha characters
+    .map(x => x.replace(/[^a-z]/gi, ''))
+    // create an array of objects with the word and its frequency
+    .reduce((totalWords, singleWord) => {
+      if (totalWords.every(element => element['word'] != singleWord)) {
         let newElement = {word: singleWord, frequency: 1};
-        totalWords[i] = newElement;
+        totalWords.push(newElement);
       } else {
-        totalWords.forEach(element => {if (element['word'] == singleWord) element['frequency']++});
+        totalWords.forEach(element => {if (element.word == singleWord) element.frequency++});
       }
       return totalWords;
     }, [])
-    .sort((a,b) => b['frequency']-a['frequency']);
+    // sort the array from highest to lowest frequency
+    .sort((a,b) => b.frequency-a.frequency);
 
-  // let content = '';
-  // for (var word in words) {
-  //   content += '<li>'+word+' - '+words[word]+'</li>';
-  // }
-  // output.innerHTML = content;
-  console.log(words);
+  let content = '';
+  // number of top words to display (user-defined)
+  let n = topNumber.value;
+  // build the table content
+  for (let i=0; i<words.length && i<n; i++) {
+    if (words[i].word)
+      content += '<tr><td>'+(i+1)+'</td>';
+      content += '<td>'+words[i].word+'</td>';
+      content += '<td>'+words[i].frequency+'</td></tr>'
+  }
+  // display output
+  output.innerHTML = content;
+  outputTable.style.display = 'table';
 });

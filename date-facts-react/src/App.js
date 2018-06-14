53dbddded77bd3f9888062ef8d5438c51e1f032d
arielbk <buchwald.kearney@gmail.com>
2018-06-14 12:48:12 +0200

    Basic styling of date facts react app

--- date-facts-react/src/App.js
+++ date-facts-react/src/App.js
@@ -1,33 +1,42 @@
 import React, { Component } from 'react';
 
+function Loading(props) {
+  return (
+    <h3>Loading...</h3>
+  )
+}
+
 function DatePicker(props) {
   return (
-    <div>
+    <div className='container date-picker'>
       <h2>Pick a date:</h2>
-      <select value={props.month} onChange={props.onMonthChange}>
-        <option value='1'>January</option>
-        <option value='2'>February</option>
-        <option value='3'>March</option>
-        <option value='4'>April</option>
-        <option value='5'>May</option>
-        <option value='6'>June</option>
-        <option value='7'>July</option>
-        <option value='8'>August</option>
-        <option value='9'>September</option>
-        <option value='10'>October</option>
-        <option value='11'>November</option>
-        <option value='12'>December</option>
-      </select>
-      <input type='Number' min='1' max='31' value={props.day} onChange={props.onDayChange} />
-      <button onClick={props.onSetToday}>Today's date</button>
+      <form className='date-picker-form'>
+        <select value={props.month} onChange={props.onMonthChange}>
+          <option value='1'>January</option>
+          <option value='2'>February</option>
+          <option value='3'>March</option>
+          <option value='4'>April</option>
+          <option value='5'>May</option>
+          <option value='6'>June</option>
+          <option value='7'>July</option>
+          <option value='8'>August</option>
+          <option value='9'>September</option>
+          <option value='10'>October</option>
+          <option value='11'>November</option>
+          <option value='12'>December</option>
+        </select>
+        <input type='Number' min='1' max='31' value={props.day} onChange={props.onDayChange} />
+        <h2> or </h2>
+        <button onClick={props.onSetToday}>Today's date</button>
+      </form>
     </div>
   )
 }
 
 function DateFact(props) {
   return (
-    <div>
-      <h3>Facts for the date</h3>
+    <div className='container'>
+      <h2>Facts for this date</h2>
       <p>
         {props.fact}
       </p>
@@ -42,7 +51,8 @@ class App extends Component {
     this.state = {
       month: nowDate.getMonth()+1,
       day: nowDate.getDate(),
-      fact: 'Loading...',
+      fact: '',
+      loading: true,
     }
     this.handleMonthChange = this.handleMonthChange.bind(this);
     this.handleDayChange = this.handleDayChange.bind(this);
@@ -56,24 +66,24 @@ class App extends Component {
 
   handleMonthChange(e) {
     const month = e.target.value;
-    console.log(month);
     this.setState({month});
     this.handleDateChange();
   }
 
   handleDayChange(e) {
     const day = e.target.value;
-    console.log(day);
     this.setState({day});
     this.handleDateChange();
   }
   
   handleDateChange() {
+    this.setState({ loading: true });
     fetch(`http://numbersapi.com/${this.state.month}/${this.state.day}/date`)
       .then(res => res.text())
       .then(data => {
-        this.setState({ fact: data })
+        this.setState({ fact: data });
       });
+    this.setState({ loading: false });
   }
 
   handleSetToday() {
@@ -89,16 +99,20 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <h1>On this date...</h1>
-        <DatePicker 
+      <div className='App'>
+        <h1 className='container'>On this date...</h1>
+        <DatePicker
           month={this.state.month} 
           day={this.state.day} 
           onMonthChange={this.handleMonthChange} 
           onDayChange={this.handleDayChange}
           onSetToday={this.handleSetToday}
         />
-        <DateFact fact={this.state.fact} />
+        { this.state.loading === true
+          ? <Loading />
+          : <DateFact fact={this.state.fact} />
+        }
+        
       </div>
     );
   }

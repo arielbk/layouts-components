66df334b01adfcc831298f17204b9c979b4f1e1f
arielbk <buchwald.kearney@gmail.com>
2018-06-26 01:16:10 +0200

    working pomodoro (with bugs)

--- pomodoro-timer/main.js
+++ pomodoro-timer/main.js
@@ -1,16 +1,21 @@
 const container = document.querySelector('.main');
 const startButton = document.querySelector('.countdown');
-const displayTime = document.querySelector('.display-time')
+const displayTime = document.querySelector('.display-time');
+const inputTime = document.querySelector('#input-time');
+const reset = document.querySelector('.reset');
 
 const timerObj = {
-  length: 5, // 5 seconds for testing
-  timeRemaining: 5, // this should be dynamic.. setNewTime will be function where this and timer.length are set
+  length: 1500, // 25*60 --- 5 minutes is default
+  timeRemaining: 1500, // this should be dynamic.. setNewTime will be function where this and timer.length are set
   timing: false, // a flag for start/pause
+  started: false,
 }
 
 function timerFunc() {
   if (timerObj.timeRemaining > 0) {
-    displayTime.innerText = timerObj.timeRemaining;
+    const minsRemaining = Math.floor((timerObj.timeRemaining % (60*60)) / 60);
+    const secsRemaining = Math.floor((timerObj.timeRemaining % 60));
+    displayTime.innerText = `Time remaining: ${minsRemaining} minutes and ${secsRemaining} seconds`;
     timerObj.timeRemaining--;
   } else {
     displayTime.innerText = 'ding';
@@ -19,16 +24,34 @@ function timerFunc() {
 
 let intervalID;
 function startPause(timer) {
+  timer.started = true;
   timer.timing = !timer.timing;
   if (timer.timing) { // if the timer is now running (after click)
     intervalID = setInterval(timerFunc, 1000);
-    startButton.innerText = 'Pause timer';
+    startButton.innerText = 'Pause';
   } else {
     console.log('clearing the interval!');
     clearInterval(intervalID);
-    startButton.innerText = 'Continue timer';
+    startButton.innerText = 'Continue';
     return;
   }
 }
 
+function setTimerLength() {
+  timerObj.length = this.value * 60;
+  if (timerObj.started === false) timerObj.timeRemaining = timerObj.length;
+}
+
+// issue here when user has timer running and presses reset then sets a new time and starts timer...
+function resetTimer() {
+  timerObj.started = false;
+  startButton.innerText = 'Start';
+  timerObj.timing = false;
+  timerObj.length = inputTime.value * 60;
+  timerObj.timeRemaining = timerObj.length;
+  displayTime.innerText = '';
+}
+
 startButton.addEventListener('click', () => startPause(timerObj));
+inputTime.addEventListener('change', setTimerLength);
+reset.addEventListener('click', resetTimer);

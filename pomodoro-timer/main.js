65d6c191a9fe2a7dc44d1a9de84d716a935bc05e
arielbk <buchwald.kearney@gmail.com>
2018-06-25 23:15:58 +0200

    pomodoro timer - finally working, setinterval returns an ID..

--- pomodoro-timer/main.js
+++ pomodoro-timer/main.js
@@ -2,33 +2,33 @@ const container = document.querySelector('.main');
 const startButton = document.querySelector('.countdown');
 const displayTime = document.querySelector('.display-time')
 
-const timer = {
+const timerObj = {
   length: 5, // 5 seconds for testing
   timeRemaining: 5, // this should be dynamic.. setNewTime will be function where this and timer.length are set
   timing: false, // a flag for start/pause
 }
 
-function startPause(timer) {
-  timer.timing = !timer.timing;
-
-  if (timer.timing) { // if the timer is now running (after click)
-    startButton.innerText = 'Pause timer';
-
-    const timerInterval = setInterval(() => {
-      if (timer.timeRemaining) { 
-        displayTime.innerText = timer.timeRemaining;
-        timer.timeRemaining--;
-      } else {
-        displayTime.innerText = 'ding!';
-        clearInterval(timerInterval);
-      }
-    }, 1000);
+function timerFunc() {
+  if (timerObj.timeRemaining > 0) {
+    displayTime.innerText = timerObj.timeRemaining;
+    timerObj.timeRemaining--;
+  } else {
+    displayTime.innerText = 'ding';
   }
-  // } else {
-  //   clearInterval(timerInterval);
-  //   startButton.innerText = 'Continue timer';
-  //   return;
-  // }
 }
 
-startButton.addEventListener('click', () => startPause(timer));
+let intervalID;
+function startPause(timer) {
+  timer.timing = !timer.timing;
+  if (timer.timing) { // if the timer is now running (after click)
+    intervalID = setInterval(timerFunc, 1000);
+    startButton.innerText = 'Pause timer';
+  } else {
+    console.log('clearing the interval!');
+    clearInterval(intervalID);
+    startButton.innerText = 'Continue timer';
+    return;
+  }
+}
+
+startButton.addEventListener('click', () => startPause(timerObj));

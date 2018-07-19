0b748ca44daf70c6eb5af78e1a1ee33f841524b5
arielbk <buchwald.kearney@gmail.com>
2018-07-19 14:04:49 +0200

    element cycler functional

--- element-cycler/main.js
+++ element-cycler/main.js
@@ -1,30 +1,61 @@
+"use strict";
+
 const array = ['jedan', 'dva', 'tri', 'ćetri', 'pet', 'šest', 'sedam', 'ossam', 'devet', 'deset'];
-const shownItem = document.querySelector('.shown-item');
+const display = document.querySelector('.display');
+
 const prevArrow = document.querySelector('.prev-arrow');
 const nextArrow = document.querySelector('.next-arrow');
 
 let index = 0;
+let elements;
 
 document.addEventListener('DOMContentLoaded', () => {
-  cycleArray(0);
+  let content = array.map(currentValue => {
+    return `<li class='hidden'>${currentValue}</li>`;
+  }).join('');
+  display.innerHTML = content;
+
+  elements = display.querySelectorAll('li');
+  elements[index].classList.remove('hidden');
 });
 
-function cycleArray(index) {
-  shownItem.innerText = array[index];
+function cycleItem(direction) {
+  let otherDirection;
+  let newIndex = index;
+  if (direction === 'left') otherDirection = 'right';
+  if (direction === 'right') otherDirection = 'left';
 
+  // REMOVE THE OLD ELEMENT
+  elements[index].classList.add(`fade-${direction}`);
+
+  setTimeout(() => {
+    elements[index].classList.remove(`fade-${direction}`);
+    elements[index].classList.add('hidden');
+
+    index = newIndex;
+  }, 300);
+
+  // ADD THE NEW ELEMENT
+  // account for special cases
+  if (index < 1 && direction === 'right') { newIndex = array.length-1; }
+  else if (index === array.length - 1 && direction === 'left') { newIndex = 0; }
+  else if (direction === 'left') { newIndex++ }
+  else if (direction === 'right') { newIndex-- }
+
+  elements[newIndex].classList.add(`fade-${otherDirection}`);
+  elements[newIndex].classList.remove('hidden');
+  setTimeout(() => elements[newIndex].classList.remove(`fade-${otherDirection}`), 300)
 }
 
-function changeItem(e) {
-  if (e.target.classList.contains('prev-arrow')) {
-    if (index === 0) index = array.length;
-    index--;
-  }
-  if (e.target.classList.contains('next-arrow')) {
-    index++;
-  }
-
-  cycleArray(index % (array.length));
-}
-
-prevArrow.addEventListener('click', changeItem)
-nextArrow.addEventListener('click', changeItem)
+prevArrow.addEventListener('click', (e) => {
+  let direction;
+  if (e.target.classList.contains('prev-arrow')) direction = 'right';
+  if (e.target.classList.contains('next-arrow')) direction = 'left';
+  cycleItem(direction);
+});
+nextArrow.addEventListener('click', (e) => {
+  let direction;
+  if (e.target.classList.contains('prev-arrow')) direction = 'right';
+  if (e.target.classList.contains('next-arrow')) direction = 'left';
+  cycleItem(direction);
+});

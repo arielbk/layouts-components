84091dbb7ea9ff4bce9450b4bb80c84598708898
arielbk <buchwald.kearney@gmail.com>
2018-09-03 22:53:11 +0200

    MongoChat refactor - ES6

--- MongoChat/server.js
+++ MongoChat/server.js
@@ -8,40 +8,38 @@ Mongo.connect('mongodb://127.0.0.1:27017', function(err, client) {
     throw err;
   }
 
-  let db = client.db('mongochat')
+  // this is a change from MongoDB 4 and up
+  const db = client.db('mongochat')
   console.log('connected to mongodb');
 
   // connect to socket.io
-  Client.on('connection', function(socket) {
-    let chat = db.collection('chats');
+  Client.on('connection', socket => {
+    const chat = db.collection('chats');
 
     // create function to send status
-    let sendStatus = function(s) {
+    const sendStatus = s => {
       socket.emit('status', s);
     }
 
     // get chat from mongo collection
-    chat.find().limit(100).sort({_id: 1}).toArray(function(err, res) {
-      if (err) {
-        throw err;
-      }
+    chat.find().limit(100).sort({_id: 1}).toArray((err, res) => {
+      if (err) throw err;
 
       // emit the messages
       socket.emit('output', res)
     })
 
     // handle input event
-    socket.on('input', function(data) {
-      let name = data.name;
-      let message = data.message;
+    socket.on('input', data => {
+      const [name, message] = data;
 
       // check for name and message
-      if (name == '' || message == '') {
+      if (!name || !message) {
         // send error status
         sendStatus('Enter a name and message!');
       } else {
         // insert message
-        chat.insertOne({name: name, message: message}, function() {
+        chat.insertOne({name: name, message: message}, () => {
           Client.emit('output', [data]);
 
           // send status object
@@ -53,9 +51,9 @@ Mongo.connect('mongodb://127.0.0.1:27017', function(err, client) {
       }
     })
     // handle clear
-    socket.on('clear', function(data) {
+    socket.on('clear', () => {
       // remove all chats from the collection
-      chat.remove({}, function(){
+      chat.remove({}, () => {
         // emit cleared
         socket.emit('cleared');
       })

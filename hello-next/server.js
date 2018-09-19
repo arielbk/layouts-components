c4243b38403ef6c92d06af06e22f3041fda9f425
arielbk <buchwald.kearney@gmail.com>
2018-09-19 23:18:44 +0200

    next js play - added now deployment

--- hello-next/server.js
+++ hello-next/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
+let port;
+(process.env.PORT) ? port = (process.env.PORT) : port = 8000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -19,9 +21,9 @@ app.prepare()
       return handle(req, res);
     });
 
-    server.listen(3000, (err) => {
+    server.listen(port, (err) => {
       if (err) throw err;
-      console.log('> Ready on http://localhost:3000');
+      console.log(`> Ready on http://localhost:${port}`);
     });
   })
   .catch((ex) => {

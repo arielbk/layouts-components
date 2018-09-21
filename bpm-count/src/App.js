e74318e8a418e2ebcf7d4ccf0f005ffa2e65495f
arielbk <buchwald.kearney@gmail.com>
2018-09-21 22:10:17 +0200

    BPM counter - setup

--- bpm-count/src/App.js
+++ bpm-count/src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { 
+  Container,
+  Button, 
+  Card } from 'reactstrap';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
+  state = {
+    bpm: 0.0
+  }
+
+  onTap = e => {
+    e.preventDefault();
+    // stuff to do when tapped
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container>
+        <h1 className="my-3">BPM Counter</h1>
+
+        {/* Getting all the pieces down before I split them up into components */}
+
+        <Button onClick={this.onTap} className="my-3">Tap</Button>
+
+        <Card>
+          <h3>BPM:</h3> {this.state.bpm}
+        </Card>
+      </Container>
     );
   }
 }
